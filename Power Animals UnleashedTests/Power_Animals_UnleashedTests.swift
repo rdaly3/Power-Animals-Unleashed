01e9a75bc3d5f1da118d5f2648bb35b1f5688705
Nauru <nauru@hbll-mlab-07.byu.edu>
2018-02-02 12:40:02 -0700

    Initial Commit

---
+++ Power Animals UnleashedTests/Power_Animals_UnleashedTests.swift
@@ -0,0 +1,36 @@
+//
+//  Power_Animals_UnleashedTests.swift
+//  Power Animals UnleashedTests
+//
+//  Created by Nauru on 2/2/18.
+//  Copyright © 2018 Scott Daly. All rights reserved.
+//
+
+import XCTest
+@testable import Power_Animals_Unleashed
+
+class Power_Animals_UnleashedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
